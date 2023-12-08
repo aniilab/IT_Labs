6680790b4ecf778b1e15483e627b7a98cca5853f
aniilab <alilinalinaa@icloud.com>
2023-12-09 01:34:12 +0200

    Minor changes

--- Lab2.GraphQL/src/fixtures/messages.ts
+++
@@ -1,6 +0,0 @@
-import { Message } from "../models/Message";
-import { user1, user2, user3 } from "./artists";
-
-export const message1 = new Message("It`s soooo good, I love it!", user1, "1")
-export const message2 = new Message("I hate fruits!", user2, "2");
-export const message3 = new Message("Does anyone has a paw wax?", user3, "3");
